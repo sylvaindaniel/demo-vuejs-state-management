08868902c86dded3bc3f564b90455ecc10aec8d1
Sylvain Daniel <sylvain.daniel@infotel.com>
2023-10-27 16:23:50 +0200

    demoVueJs StateManagement

---
+++ src/components/GroceryList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1> {{ pageTitle }}</h1>
+        <ul>
+            <ListItem
+                v-for="item in list"
+                :key="item"
+                :item="item"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import  ListItem from "./ListItem"
+
+export default {
+    name: "GroceryList",
+    components: {
+        ListItem
+    },
+    data() {
+        return {
+            list: ['pommes', 'bananes', 'cerises'],
+            pageTitle:'Ma liste de courses'
+        }
+    }
+
+}
+</script>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="checkbox" v-model="isPurchased" />
+        <label>{{ item }}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    props: {
+        item: String
+    },
+    data() {
+        return {
+            isPurchased: false
+        }
+    }
+}
+</script>
